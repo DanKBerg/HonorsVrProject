bc02f7935881b84bbd2854401a13e3b78ce3b47c
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-02-27 23:04:27 -0500

    Teleportation Works

---
+++ Honors Thesis VR Project/Assets/Scripts/DevelopmentQuit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DevelopmentQuit : MonoBehaviour
+{
+    public XRController leftHand;
+    public InputHelpers.Button quitButton;
+    public float activationThreshold;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckIfActivated(leftHand))
+        {
+            Application.Quit();
+            Debug.Log("Ended Session!");
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, quitButton, out bool isActivated);
+        return isActivated;
+    }
+}
