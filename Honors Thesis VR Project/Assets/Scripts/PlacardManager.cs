386af2ed2992dab3a06d018c13244dbbb85d8ac6
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-03-14 21:25:35 -0400

    I dunno

---
+++ Honors Thesis VR Project/Assets/Scripts/PlacardManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacardManager : MonoBehaviour
+{
+    public List<GameObject> placards;
+    private int placardNumber = 0;
+    public int lastPlacardIndex;
+
+    public void NextPlacard()
+    {
+        if(placardNumber < lastPlacardIndex)
+        {
+            placards[placardNumber].SetActive(false);
+
+            placards[placardNumber + 1].SetActive(true);
+
+            placardNumber += 1;
+        }
+    }
+
+    public void PreviousPlacard()
+    {
+        if(placardNumber > 0)
+        {
+            placards[placardNumber].SetActive(false);
+
+            placards[placardNumber - 1].SetActive(true);
+
+            placardNumber -= 1;
+        }
+        
+    }
+}
